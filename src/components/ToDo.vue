38f3a9c17978c4ef89216024b8864be2811dc718
antoineantix <lucas@et.esiea.fr>
2019-09-10 11:55:36 +0200

    Adding new features

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -23,17 +23,15 @@
 
             <v-col cols="12" md="4">
               <v-btn v-on:click="addTodo">Add</v-btn>
+              <v-btn v-on:click="seen">Del</v-btn>
             </v-col>
 
             <v-col cols="12" md="4">
               <ol>
                   <li v-for="(item, i) in todoList" :key="i">
                     {{ item.title }} : {{item.content}}
-                    <v-col cols="12" md="4">
-                      <v-btn v-on:click="delTodo(i)">Del</v-btn>
-                    </v-col>
+                    <v-btn v-if="seen_button" v-on:click="delTodo(i)">Del</v-btn>
                   </li>
-                
               </ol>
             </v-col>
 
@@ -50,6 +48,7 @@ export default {
     valid: false,
     title: '',
     content: '',
+    seen_button: false,
     todoList: []
   }),
   methods: {
@@ -59,9 +58,17 @@ export default {
         content: this.content
       })
       console.log(JSON.stringify(this.todoList))
+      this.seen_button = false
     },
     delTodo (i) {
-      this.todoList.splice(i, 1)
+      var x = window.confirm('Voulez vous suprimer cet élèment de la liste ?')
+      if (x) {
+        this.todoList.splice(i, 1)
+        this.seen_button = false
+      }
+    },
+    seen () {
+      this.seen_button = true
     }
   }
 }
